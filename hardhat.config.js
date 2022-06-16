28117e02af9c35953e4c0f521d8e027c90de081d
gmluqa <gmluqa@protonmail.com>
2022-06-16 14:12:50 +0300

    Added compiler 0.6.6

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,58 +1,60 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      // blockConfirmations: 1,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            chainId: 4,
+            // blockConfirmations: 6,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY], // is in array because filed asks for array format
+            saveDeployments: true,
+        },
     },
-    localhost: {
-      chainId: 31337,
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+        },
     },
-    rinkeby: {
-      chainId: 4,
-      // blockConfirmations: 6,
-      url: RINKEBY_RPC_URL,
-      accounts: [PRIVATE_KEY], // is in array because filed asks for array format
-      saveDeployments: true,
+    gasReporter: {
+        enabled: false,
     },
-  },
-  etherscan: {
-    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
-      rinkeby: ETHERSCAN_API_KEY,
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }], // compilers in this syntax
     },
-  },
-  gasReporter: {
-    enabled: false,
-  },
-  solidity: "0.8.7",
-  namedAccounts: {
-    // Named accounts module
-    deployer: {
-      default: 0, // defaults to hardhat account[0]
-      1: 0,
+    namedAccounts: {
+        // Named accounts module
+        deployer: {
+            default: 0, // defaults to hardhat account[0]
+            1: 0,
+        },
+        player: {
+            default: 1, // defaults to hardhat account[1]
+        },
     },
-    player: {
-      default: 1, // defaults to hardhat account[1]
+    mocha: {
+        timeout: 200000, // 200 seconds max for promises
     },
-  },
-  mocha: {
-    timeout: 200000, // 200 seconds max for promises
-  },
-};
+}
