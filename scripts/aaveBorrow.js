6230cc97004cad9be77c1b1fac04e88050d3f1c3
gmluqa <gmluqa@protonmail.com>
2022-06-18 10:44:06 +0300

    Removed interface cuz  is already in nodemodule

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,8 +1,30 @@
 // No need for deploy since we're just interacting with already existing contracts
+const { ethers, getNamedAccounts } = require("hardhat")
 const { getWeth } = require("./getWeth")
 
 async function main() {
     await getWeth()
+    const { deployer } = await getNamedAccounts()
+    // abi, address
+
+    // Lending pool address: 0xb53c1a33016b2dc2ff3653530bff1848a515c8c5 https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`Lending pool address ${lendingPool.address}`)
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xb53c1a33016b2dc2ff3653530bff1848a515c8c5",
+        account
+    ) // getContractAt is a hardhat extension function of ethers // https://hardhat.org/plugins/nomiclabs-hardhat-ethers
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        lendingPoolAddress,
+        account
+    )
+    return lendingPool
 }
 
 main()
